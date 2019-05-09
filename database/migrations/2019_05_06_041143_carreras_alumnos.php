2a39c58987421a584fc596d6e6e7e36d0223b1b4
Airam Duarte <alexandra.duarte@uabc.edu.mx>
2019-05-08 20:11:41 -0700

    modificacion carreras_alumnos

--- database/migrations/2019_05_06_041143_carreras_alumnos.php
+++ database/migrations/2019_05_06_041143_carreras_alumnos.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CarreraAlumno extends Migration
+class CarrerasAlumnos extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CarreraAlumno extends Migration
      */
     public function up()
     {
-        Schema::create('carrera_alumno', function(Blueprint $table)
+        Schema::create('carreras_alumnos', function(Blueprint $table)
         {
             $table->integer('carrera_noUnico')->unsigned();
            $table->foreign('carrera_noUnico')->references('noUnico')->on('carrera')->onDelete('cascade');
@@ -31,6 +31,6 @@ class CarreraAlumno extends Migration
     public function down()
     {
         //
-        Schema::dropIfExists('carrera_alumno');
+        Schema::dropIfExists('carreras_alumnos');
     }
 }
