0d8e59556b034f084633d70434cc0b27c662e401
Airam Duarte <alexandra.duarte@uabc.edu.mx>
2019-05-26 14:17:13 -0700

    cambios aws

--- routes/api.php
+++ routes/api.php
@@ -46,5 +46,7 @@ Route::post('login', 'API\UserController@login');
 Route::post('register', 'API\UserController@register');
 Route::group(['middleware' => 'auth:api'], function(){
 Route::post('details', 'API\UserController@details');
+
+
 });
 
